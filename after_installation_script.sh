a2c6bccf5638cabe56db290c90cf04aeffa5e46c
Brenddon Gontijo Furtado <brenddongontijo@msn.com>
2016-08-26 10:24:55 -0300

    Fix installation script

--- after_installation_script.sh
+++ after_installation_script.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-# Script to download all necessary cookbooks for Chef and run vagrant up.
 
-mkdir cookbooks && cd cookbooks/
+sudo apt-get install postgresql python-psycopg2 libpq-dev -y
 
-git clone https://github.com/chef-cookbooks/apt.git
-git clone https://github.com/chef-cookbooks/build-essential.git
-git clone https://github.com/chef-cookbooks/chef_handler.git
-git clone https://github.com/chef-cookbooks/compat_resource.git
-git clone https://github.com/chef-cookbooks/dmg.git
-git clone https://github.com/chef-cookbooks/git.git
-git clone https://github.com/chef-cookbooks/mingw.git
-git clone https://github.com/chef-cookbooks/openssl.git
-git clone https://github.com/chef-cookbooks/vim.git
-git clone https://github.com/chef-cookbooks/windows.git
-git clone https://github.com/chef-cookbooks/yum-epel.git
-git clone https://github.com/chef-cookbooks/yum.git
-git clone https://github.com/daptiv/seven_zip.git
-git clone https://github.com/poise/python.git
-git clone https://github.com/sethvargo/chef-sugar.git
+ADMIN="admin"
+SMEDB="smeunb"
 
-cd .. && vagrant up
+sudo su - postgres -c "createuser vagrant --no-superuser --no-createdb --no-createrole" || true
+sudo su - postgres -c "createuser $ADMIN --no-superuser --createdb --no-createrole" || true
+sudo su - postgres -c "createdb $SMEDB -O $ADMIN" || true
 
-vagrant ssh -c "/vagrant/after_installation.sh"
-vagrant ssh
+cd /vagrant/SME_UnB
+
+sudo pip install -r requirements.txt
+
+python manage.py migrate
