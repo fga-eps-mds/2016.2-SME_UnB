28619a42a077a00a5d50d2e15c4595e42f83b605
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-08 19:18:21 -0300

    adding urls for login, home, dashboard and their respective templates

---
+++ SME_UnB/users/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^$', views.login_view, name="home"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
+    ]
