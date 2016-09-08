cc020a5d51f2076991b9ef055e31436922466d6b
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-08 19:15:08 -0300

    adding users models

--- SME_UnB/users/models.py
+++ SME_UnB/users/models.py
@@ -1,5 +1,46 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.translation import gettext as _
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+class EmailUserManager(BaseUserManager):
+    def create_user(self, *args, **kwargs):
+        email = kwargs["email"]
+        email = self.normalize_email(email)
+        password = kwargs["password"]
+        kwargs.pop("password")
+
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+
+        user = self.model(**kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, *args, **kwargs):
+        user = self.create_user(**kwargs)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name=_('Email address'),
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name=_('Nome'),
+        max_length=50,
+        blank=False,
+        help_text=_('Inform your name'),
+    )
+    last_name = models.CharField(
+        verbose_name=_('Sobrenome'),
+        max_length=50,
+        blank=False,
+        help_text=_('Inform your last name'),
+    )
+    USERNAME_FIELD = 'email'
+    objects = EmailUserManager()
