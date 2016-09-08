f59b7ee4c313cfb56789819f420ad2d6a2ccb4c6
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-08 19:23:25 -0300

    views of login

--- SME_UnB/users/views.py
+++ SME_UnB/users/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+from django.contrib.auth.decorators import login_required
+
+from django.core.urlresolvers import reverse_lazy, reverse
+
+"""from .forms import CustomUserCreationForm"""
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
+
+
+def login_view(request, *args, **kwargs):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('users:dashboard'))
+
+    kwargs['extra_context'] = {'next': reverse('users:dashboard')}
+    kwargs['template_name'] = 'users/login.html'
+    return login(request, *args, **kwargs)
+
+
+def logout_view(request, *args, **kwargs):
+    kwargs['next_page'] = reverse('index')
+    return logout(request, *args, **kwargs)
+
+"""
+class RegistrationView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = "users/register.html"
+"""
