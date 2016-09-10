56535c6ae85f233cec07d894400789e91dfa9f41
Ulysses <ulysses3353@gmail.com>
2016-09-10 10:36:11 -0300

    edit template implemented

--- SME_UnB/users/urls.py
+++ SME_UnB/users/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     url(r'^login/$', views.login_view, name="login"),
     url(r'^logout/$', views.logout_view, name="logout"),
     url(r'^register/', views.register, name="register"),
+    url(r'^list_user/', views.list_user, name="list_user"),
+    url(r'^edit_user/(?P<user_id>[0-9]+)/',views.edit_user, name="edit_user"),
+
+
 
     ]
