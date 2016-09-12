927fd184e6145306f2f643043401eccd1919ba60
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-12 13:30:50 -0300

    login update

--- SME_UnB/users/urls.py
+++ SME_UnB/users/urls.py
@@ -5,7 +5,11 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name="dashboard"),
-    url(r'^$', views.login_view, name="home"),
-    url(r'^login/$', views.login_view, name="login"),
+    url(r'^$', views.show_login, name="home"),
+    url(r'^login/$', views.show_login, name="login"),
     url(r'^logout/$', views.logout_view, name="logout"),
+    url(r'^register/', views.register, name="register"),
+    url(r'^list_user/', views.list_user, name="list_user"),
+    url(r'^edit_user/(?P<user_id>[0-9]+)/',views.edit_user, name="edit_user"),
+
     ]
--- SME_UnB/users/views.py
+++ SME_UnB/users/views.py
@@ -1,15 +1,52 @@
-from django.shortcuts import render
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import logout
 from django.contrib.auth.decorators import login_required
 
 from django.core.urlresolvers import reverse_lazy, reverse
 
+from users.models import MyUser
+
 """from .forms import CustomUserCreationForm"""
+
+def show_login(request):
+    if request.method == "GET":
+        return render(request, "users/login.html")
+    else:
+        context = make_login(request)
+
+        if context.get('is_logged'):
+            return redirect(reverse("users:dashboard"))
+        else:
+            return render(request, "users/login.html", context)
+
+def make_login(request):
+    form = request.POST
+    email = form.get('email')
+    password = form.get('password')
+
+    user = authenticate(email=email, password=password)
+    is_logged = False
+
+    if user is not None:
+        login(request, user)
+        message = "Logged"
+        is_logged = True
+    else:
+        message = "Incorrect user"
+
+    context = {
+        "is_logged": is_logged,
+        "message": message,
+    }
+
+    return context
+
+
 
 
 def home(request):
@@ -20,22 +57,59 @@ def dashboard(request):
     return render(request, 'users/dashboard.html')
 
 
-def login_view(request, *args, **kwargs):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('users:dashboard'))
 
-    kwargs['extra_context'] = {'next': reverse('users:dashboard')}
-    kwargs['template_name'] = 'users/login.html'
-    return login(request, *args, **kwargs)
 
 
 def logout_view(request, *args, **kwargs):
     kwargs['next_page'] = reverse('index')
     return logout(request, *args, **kwargs)
 
-"""
-class RegistrationView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('users:login')
-    template_name = "users/register.html"
-"""
+def register(request):
+
+    if request.method == "GET":
+        return render(request,'userRegister/register.html')
+    else:
+        form = request.POST
+        first_name = form.get('first_name')
+        last_name =  form.get('last_name')
+        password = form.get('password')
+        email = form.get('email')
+
+
+
+
+
+        try:
+            user = MyUser.objects.create_user(first_name=first_name,last_name=last_name,password=password,email=email)
+        except:
+            return render(request,'userRegister/register.html', {'falha':'Email invalido!'})
+
+        user.save()
+
+        return render(request,'users/home.html')
+
+def list_user(request):
+
+    users = MyUser.objects.all()
+    return render(request,'users/list_user.html',{'users':users})
+
+def edit_user(request,user_id):
+
+    user = MyUser.objects.get(id=user_id)
+    if request.method == "GET":
+        return render(request,'users/edit_user.html',{'user':user})
+    else:
+        form = request.POST
+        first_name = form.get('first_name')
+        last_name =  form.get('last_name')
+        password = form.get('password')
+        email = form.get('email')
+
+        user.first_name = first_name
+        user.last_name  =last_name
+        user.set_password(password)
+        user.email = email
+
+        user.save()
+
+        return render(request,'users/edit_user.html',{'info':'usuario modificado com sucesso'})
