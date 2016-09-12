a8918bbaec7ea440c385162d1980b6c129ecf947
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-12 13:30:50 -0300

    login update
    Conflicts:
    	SME_UnB/users/templates/users/dashboard.html
    	SME_UnB/users/urls.py
    	SME_UnB/users/views.py

--- SME_UnB/users/views.py
+++ SME_UnB/users/views.py
@@ -1,13 +1,53 @@
-from django.shortcuts import render
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import logout
 from django.contrib.auth.decorators import login_required
 
 from django.core.urlresolvers import reverse_lazy, reverse
+
+from users.models import MyUser
+
+"""from .forms import CustomUserCreationForm"""
+
+def show_login(request):
+    if request.method == "GET":
+        return render(request, "users/login.html")
+    else:
+        context = make_login(request)
+
+        if context.get('is_logged'):
+            return redirect(reverse("users:dashboard"))
+        else:
+            return render(request, "users/login.html", context)
+
+def make_login(request):
+    form = request.POST
+    email = form.get('email')
+    password = form.get('password')
+
+    user = authenticate(email=email, password=password)
+    is_logged = False
+
+    if user is not None:
+        login(request, user)
+        message = "Logged"
+        is_logged = True
+    else:
+        message = "Incorrect user"
+
+    context = {
+        "is_logged": is_logged,
+        "message": message,
+    }
+
+    return context
+
+
+
 
 def home(request):
     return render(request, 'users/home.html')
@@ -39,7 +79,6 @@ def register(request):
         last_name =  form.get('last_name')
         password = form.get('password')
         email = form.get('email')
-        
         try:
             user = MyUser.objects.create_user(first_name=first_name,last_name=last_name,password=password,email=email)
         except: 
