bd42bafdb321e1900da07a9bdb0ddf87f2692b03
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-19 21:32:15 -0300

    Adding test to correct login

--- SME_UnB/users/tests.py
+++ SME_UnB/users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+import unittest
 
-# Create your tests here.
+class TestLoginView(unittest.TestCase):
+    def test_correct_login(self):
+        client = Client()
+        response = client.post(
+            '/accounts/login/',
+            {"username": 'admin', 'password': 'tiago1107' }
+        )
+
+        self.assertEqual(200, response.status_code)
