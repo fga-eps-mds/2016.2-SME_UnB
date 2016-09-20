7137a5ab8bcf187847733798ac6ea8dad1414bfa
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-19 22:09:13 -0300

    Adding some tests to login

--- SME_UnB/users/tests.py
+++ SME_UnB/users/tests.py
@@ -1,13 +1,38 @@
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth.models import User
 import unittest
 
 class TestLoginView(unittest.TestCase):
-    def test_correct_login(self):
-        client = Client()
-        response = client.post(
+    def setUp(self):
+        self.client = Client()
+
+    def test_getting_page_login(self):
+        response = self.client.post(
             '/accounts/login/',
-            {"username": 'admin', 'password': 'tiago1107' }
+            {"username": 'temporary', 'password': 'temporary' }
         )
 
         self.assertEqual(200, response.status_code)
+
+    def test_getting_wrong_page_login(self):
+        response = self.client.post(
+            '/accounts/ladfogin/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(404, response.status_code)
+
+    def test_getting_page_home(self):
+        response = self.client.get(
+            '/accounts/dashboard/',
+        )
+
+        self.assertEqual(302, response.status_code)
+
+    def test_getting_wrong_page_home(self):
+        response = self.client.get(
+            '/accounts/dash/',
+        )
+
+        self.assertEqual(404, response.status_code)
