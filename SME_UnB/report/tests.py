5f3d4685d2fbd74674b7242822a98bfe8ce0c530
Vinicius Bandeira <bandeiravinicius0@live.com>
2016-10-14 23:15:47 -0300

    Creating test for graphic_filter Signed-off-by: Ulysses Lara<ulysses3353@gmail.com>

--- SME_UnB/report/tests.py
+++ SME_UnB/report/tests.py
@@ -9,7 +9,7 @@ class TestReportView(unittest.TestCase):
 
     def test_getting_page_report(self):
         response = self.client.post(
-            '/reports/report/',
+            '/reports/report/1/',
             {"username": 'temporary', 'password': 'temporary' }
         )
 
@@ -18,6 +18,22 @@ class TestReportView(unittest.TestCase):
     def test_getting_wrong_page_report(self):
         response = self.client.post(
             '/reports/reports/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(404, response.status_code)
+
+    def test_getting_page_filter(self):
+        response = self.client.post(
+            '/reports/transductors_filter/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(200, response.status_code)
+
+    def test_getting_wrong_page_filter(self):
+        response = self.client.post(
+            '/reports/transductors_filter/',
             {"username": 'temporary', 'password': 'temporary' }
         )
 
