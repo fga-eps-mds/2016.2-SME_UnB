e2bf14c998223cd3d2cc8d25ceb0218b0fa8e371
thiago kairala <thiago kairala>
2016-10-21 11:18:43 -0200

    Finished to create notificiations
    	Signed-off-by: thiago kairala <thiagor.kairala@gmail.com>
    	Signed-off-by: Caio gabriel

---
+++ SME_UnB/alerts/models.py
@@ -0,0 +1,62 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from enum import Enum
+import pusher
+from django.core import serializers
+from datetime import datetime
+
+# Create your models here.
+
+class Alert(models.Model):
+    status = models.BooleanField()
+    description = models.CharField(max_length=50)
+    creation_date = models.DateTimeField(default=datetime.now())
+    local = models.CharField(max_length=50)
+    priority = models.IntegerField()
+    user =  models.ForeignKey(User)
+
+    class Meta:
+        permissions = (
+            ("can_check_alerts", "Can Check Alerts"),
+        )
+
+class UserNotification(object):
+
+    @staticmethod
+    def send_notification(id_user, message, local, priority):
+        APP_ID = '259103'
+        APP_KEY = '01346f7690ac693e7adb'
+        APP_SECRET = '20bf35aeec654118b90c'
+        NOTIFICATION_PREFIX = 'notification_'
+        CREATE_EVENT = 'create'
+
+        alertJson = UserNotification.__create_alert__(id_user, message, local, priority)
+
+        pusher_client = pusher.Pusher(
+          app_id=APP_ID,
+          key=APP_KEY,
+          secret=APP_SECRET,
+          ssl=True
+        )
+
+        pusher_client.trigger(NOTIFICATION_PREFIX + str(id_user), CREATE_EVENT, alertJson)
+
+    @staticmethod
+    def __create_alert__(id_user, message, local, priority):
+        user = User.objects.get(id = id_user)
+
+        alert = Alert(user = user, status = False, description = message, local=local, priority = priority)
+
+        alertJson = serializers.serialize('json', [ alert, ])
+
+        alert.save()
+
+        return alertJson
+
+
+class PriorityOfNotification(Enum):
+    HIGH = 3
+    MEDIUM = 2
+    LOW = 1
