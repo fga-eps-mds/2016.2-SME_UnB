1e21cf1d6c5abf969fb3d3388788125478d491fc
IagoCarvalho <IagoVC_2012@hotmail.com>
2016-11-09 18:41:13 -0200

    transductor flake8 indentation

--- SME_UnB/transductor/tests/test_models.py
+++ SME_UnB/transductor/tests/test_models.py
@@ -11,7 +11,8 @@ class EnergyTransductorTestCase(TestCase):
         t_model.name = "TR 4020"
         t_model.internet_protocol = "UDP"
         t_model.serial_protocol = "Modbus RTU"
-        t_model.register_addresses = [68, 70, 72, 74, 76, 78, 80, 82, 84, 88, 90, 92]
+        t_model.register_addresses = [68, 70, 72, 74, 76, 78,
+                                      80, 82, 84, 88, 90, 92]
         t_model.save()
 
         transductor = EnergyTransductor()
@@ -52,6 +53,7 @@ class EnergyTransductorTestCase(TestCase):
         self.assertEqual(t_model.name, t_model.__str__())
 
     def test_str_from_energy_transductor(self):
-        transductor = EnergyTransductor.objects.get(ip_address="111.111.111.111")
+        transductor = EnergyTransductor.objects. \
+                      get(ip_address="111.111.111.111")
 
         self.assertEqual(transductor.description, transductor.__str__())
