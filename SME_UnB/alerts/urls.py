5dd0aa827bc8b7c376c3740c299f8c89fe776f30
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-11-10 10:38:16 -0200

    moving default project directory
    Signed-off-by: CaioGabrielAraujo <caiohgabriel@gmail.com>
    Signed-off-by: luis henrique <luishng123@gmail.com>

--- SME_UnB/alerts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'alerts'
-urlpatterns = [
-    url(r'^alerts/', views , name="alert"),
-]
--- SME_UnB/alerts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from transductor.models import EnergyTransductor
-
-# Create your views here.
-
-
-#def index(request):
- #   return render(request, "alerts/index.html")
