463f24c136a70f0fdb27bed657f5f67387d3c788
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-11-10 10:47:17 -0200

    changing default project directory
    Signed-off-by: CaioGabrielAraujo <caiohgabriel@gmail.com>
    Signed-off-by: luis henrique <luishng123@gmail.com>

---
+++ src/report/tests.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+import unittest
+
+class TestReportView(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_getting_page_report(self):
+        response = self.client.post(
+            '/reports/report/1/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(200, response.status_code)
+
+    def test_getting_wrong_page_report(self):
+        response = self.client.post(
+            '/reports/reports/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(404, response.status_code)
+
+    def test_getting_pdf(self):
+        logged_in = self.client.login(username='testuser', password='12345')
+        response = self.client.get('/reports/open_pdf/')
+
+        self.assertEqual(200, response.status_code)
+
+    def test_getting_page_filter(self):
+        response = self.client.post(
+            '/reports/transductors_filter/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(200, response.status_code)
+
+    def test_getting_wrong_page_filter(self):
+        response = self.client.post(
+            '/reports/transductors_filter/',
+            {"username": 'temporary', 'password': 'temporary' }
+        )
+
+        self.assertEqual(404, response.status_code)
---
+++ src/users/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^$', views.show_login, name="home"),
+    url(r'^login/$', views.show_login, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
+    url(r'^register/', views.register, name="register"),
+    url(r'^list_user_edit/', views.list_user_edit, name="list_user_edit"),
+    url(r'^list_user_delete/', views.list_user_delete, name="list_user_delete"),
+    url(r'^edit_user/(?P<user_id>[0-9]+)/',views.edit_user, name="edit_user"),
+    url(r'^delete_user/(?P<user_id>[0-9]+)/',views.delete_user, name="delete_user"),
+    url(r'^self_edit/',views.self_edit_user, name="self_edit_user"),
+    url(r'^logging_list/',views.logging_list, name="logging_list"),
+]
